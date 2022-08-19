ea693c0b9d16c04ac68742d432bff88129006b5d
Shahid Ghafoor <shahidg@live.co.uk>
2022-08-19 02:13:23 +0100

    property binding to send array list to component

--- src/app/game/game-card/game-card.component.ts
+++ src/app/game/game-card/game-card.component.ts
@@ -1,21 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-game-card',
   templateUrl: 'game-card.component.html',
   styleUrls: ['game-card.component.css'],
 })
 export class GameCardComponent {
-  Game: any = {
-    Id: 1,
-    Genre: 'FPS',
-    Title: 'MW2',
-    Price: 59.99,
-    Age: 18,
-    Info: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tmollit anim id est laborum."',
-  };
-  constructor() {
-    this.Game.Id;
-  }
+  @Input() Game: any;
+
+  constructor() {}
 
   ngOnInit(): void {}
 }
--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game-list',
   templateUrl: './game-list.component.html',
-  styleUrls: ['./game-list.component.css']
+  styleUrls: ['./game-list.component.css'],
 })
 export class GameListComponent implements OnInit {
+  games: Array<any> = [
+    {
+      Id: 1,
+      Genre: 'FPS',
+      Title: 'MW2',
+      Price: 59.99,
+      Age: 18,
+      Info: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tmollit anim id est laborum."',
+    },
+    {
+      Id: 2,
+      Genre: 'FPS',
+      Title: 'Halo 5',
+      Price: 69.99,
+      Age: 18,
+      Info: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tmollit anim id est laborum."',
+    },
+    {
+      Id: 1,
+      Genre: 'RPG',
+      Title: 'Sea of Theives',
+      Price: 49.99,
+      Age: 18,
+      Info: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tmollit anim id est laborum."',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
