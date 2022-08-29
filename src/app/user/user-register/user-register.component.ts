4acfa0b004b69bd24bb39d92b32086bc12583539
Shahid Ghafoor <shahidg@live.co.uk>
2022-08-29 21:24:21 +0100

    added registration with reactive forms + valid..

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, NgForm } from '@angular/forms';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, NgForm, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -9,8 +9,41 @@ import { FormGroup, NgForm } from '@angular/forms';
 export class UserRegisterComponent implements OnInit {
   registerForm!: FormGroup;
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
+    this.registerForm = new FormGroup(
+      {
+        name: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(32),
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(9),
+        ]),
+        confirmPass: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(9),
+        ]),
+        mobile: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(11),
+        ]),
+      },
+      this.verifyPasswordMatch
+    );
+
+    this.onSub();
   }
 
-  ngOnInit() {}
+  onSub() {
+    console.log(this.registerForm);
+  }
+
+  verifyPasswordMatch(f: AbstractControl): ValidationErrors|null {
+    return f.get('password')?.value === f.get('confirmPass')?.value ? null :
+      { notmatched: true }
+  }
 }
